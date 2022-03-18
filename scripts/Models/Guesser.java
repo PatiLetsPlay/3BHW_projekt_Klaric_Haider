de60d34e239c734f1ba3e921d186811d80f60b48
PatiLetsPlay <PatiLetsPlay77@hotmail.com>
2022-03-18 12:13:10 +0100

    Guesser vererbte Klasse hinzugefügt

---
+++ scripts/Models/Guesser.java
@@ -0,0 +1,22 @@
+package Models;
+
+public class Guesser extends Person{
+
+    private int _score;
+
+    public int getScore(){ return this._score; }
+    public void setScore(int score){ this._score = score; }
+
+    public Guesser(){
+        this(0,"","", 0);
+    }
+    public Guesser(int id, String firstname, String lastname, int score){
+        super(id,firstname,lastname);
+        this.setScore(score);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "Score: " + getScore() + " \n";
+    }
+}
