2d0c2ac4e9e2b912ea6a99b771bfd33e02de8edc
PatiLetsPlay <PatiLetsPlay77@hotmail.com>
2022-04-22 13:16:19 +0200

    Alter einfach SQL

--- SQL/vokabeln_abfrage.sql
+++ SQL/vokabeln_abfrage.sql
@@ -4,7 +4,7 @@ use Vokabeln_Abfragen;
 create table vokabelabfrage_admin(
 	id int unsigned not null auto_increment,
     firstname varchar(50) not null, 
-    lastaname varchar(50) not null, 
+    lastname varchar(50) not null, 
     usercode int unique not null,
     
     constraint id_PK primary key (id)
@@ -12,14 +12,13 @@ create table vokabelabfrage_admin(
 
 create table vokabelabfrage_guesser(
 	id int unsigned not null auto_increment,
-    firstname varchar(50) not null, 
-    lastaname varchar(50) not null, 
-    guesser_password varchar(100) not null,
+    firstname varchar(50) not null,
+    lastname varchar(50) not null,
 	score int unsigned not null,
     
     constraint id_PK primary key (id)
 );
-#drop table vokabelabfrage_admin;
-#drop table vokabelabfrage_guesser;
-select * from vokabelabfrage_guesser;
+#drop table if exists vokabelabfrage_admin;
+#drop table if exists vokabelabfrage_guesser;
+select * from vokabelabfrage_guesser where firstname = "Patrick" and lastname = "Klaric";
 select * from vokabelabfrage_admin;
